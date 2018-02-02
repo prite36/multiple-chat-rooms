9c3cc761b516a789f5711aeb62e7a6649a9e3dd1
DESKTOP-V02D53O\champ <champtnpc@gmail.com>
2018-02-02 11:39:27 +0700

    fontend success

--- webchat/src/App.vue
+++ webchat/src/App.vue
@@ -1,18 +1,55 @@
 <template>
   <div id="app">
-    <div v-for="(data, index) in countPeople" :key="index">
-      {{index}}  {{data}}
-    </div>
-    Your Name  <input type="text" name="" v-model="userName">
-    <button type="button" name="button" @click="start('room1')">Room1</button>
-    <button type="button" name="button" @click="start('room2')">Room2</button>
-    <button type="button" name="button" @click="start('room3')">Room3</button>
-
-    <br><input type="text" name="" v-model="myMessage">
-    <button type="button" name="button" @click="sendMessage()">send</button>
-    <br>
-    <div v-for="(chat, index) in allChat" :key="index">
+      <div v-if="!statusmodal" height="100vh">
+        <div class="btn">
+          <center>
+            <a class="button is-primary" v-if="countPeople.room1 < 10" @click="start('room1')">room1 {{countPeople.room1}}/10</a>
+            <a class="button is-primary" v-else disable>room1 {{countPeople.room1}}/10 (Full)</a>
+            <a class="button is-primary" v-if="countPeople.room2 < 10" @click="start('room2')">room2 {{countPeople.room2}}/10</a>
+            <a class="button is-primary" v-else disable>room2 {{countPeople.room2}}/10 (Full)</a>
+            <a class="button is-primary" v-if="countPeople.room3 < 10" @click="start('room3')">room3 {{countPeople.room3}}/10</a>
+            <a class="button is-primary" v-else disable>room3 {{countPeople.room3}}/10 (Full)</a>
+          </center>
+        </div>
+      </div>
+    <b-modal v-else :canCancel="false" :active.sync="statusmodal">
+      <div class="modal-card animation-content">
+        <section class="modal-card-body is-titleless">
+          <div class="media">
+            <div class="media-content">
+              <p>What's your name?</p>
+              <br>
+              <div class="field">
+                <div class="control">
+                  <input required="required" placeholder="Your name" maxlength="10" v-model="userName" class="input">
+                </div>
+                <p class="help is-danger"></p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-primary" @click="statusmodal = false">
+            Submit
+          </button>
+        </footer>
+      </div>
+    </b-modal>
+    <div class="content">
+      <p v-for="(chat, index) in allChat" :key="index">
         {{chat}}
+        <!-- <p  v-if="key === userName" align="right">{{chat}}</p> -->
+      </p>
+    </div>
+    <div class="footfix" v-show="selectRoom">
+      <footer>
+        <table class="chatbox" width="100%">
+          <tr>
+            <td><input class="input is-rounded" required="required" placeholder="Your message" maxlength="256" v-model="myMessage"></td>
+            <td>&nbsp;&nbsp;<button class="button is-primary" @click="sendMessage()" icon="send" size="is-medium">send</button></td>
+          </tr>
+        </table>
+      </footer>
     </div>
   </div>
 </template>
@@ -22,8 +59,9 @@ export default {
   name: 'App',
   data () {
     return {
-      userCount: 0,
+      statusmodal: true,
       userName: '',
+      userCount: 0,
       selectRoom: null,
       allChat: [],
       countPeople: null,
@@ -32,13 +70,8 @@ export default {
   },
   sockets: {
     connect () {
-
-    },
-    disconnect () {
-      console.log('server Down')
     },
     updatechat (data) {
-      console.log(data.data)
       this.allChat.push(`${data.userName} : ${data.data}`)
     },
     updateusers (data) {
@@ -66,19 +99,33 @@ export default {
         this.myMessage = ''
       }
     }
-  },
-  mounted () {
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 93vh;
+}
+.chatbox {
+
+}
+.footfix {
+  padding-left: 5%;
+  padding-right: 1%;
+  width: 100%;
+  display:scroll;
+  position:fixed;
+  bottom:2%;
+}
+.content {
+  padding-left: 2%;
+  padding-right: 2%;
+  overflow: scroll;
+  max-height: 70vh;
+  display: block;
+}
+.btn {
+  padding-top: 2%;
 }
 </style>
--- webchat/src/main.js
+++ webchat/src/main.js
@@ -4,8 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueSocketio from 'vue-socket.io'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+
+Vue.use(Buefy)
 Vue.config.productionTip = false
-Vue.use(VueSocketio, 'http://192.168.1.212:3000')
+Vue.use(VueSocketio, 'http://192.168.1.3:3000')
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
